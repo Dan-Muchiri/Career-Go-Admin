308c540ada9036401036ffe08a6c7d69e6816b44
Dan-Munene <danspmunene@gmail.com>
2024-05-17 16:01:40 +0300

    Added jobseeker left side

--- src/components/UserLeftSide.jsx
+++ src/components/UserLeftSide.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const UserLeftSide = ({ user }) => {
     return (
         <div className="left-side">
-            {user.employer.length > 0 && user.employer.map((employer, index) => (
+            {user.role === 'employer' && user.employer.length > 0 && user.employer.map((employer, index) => (
                 <div key={index} className='employer-left-side'>
                     <div className="profile-pic">
                         <img src={'/Employerlogo.png'} alt={`${user.username}`} className="employer-picture"/>
@@ -18,7 +18,29 @@ const UserLeftSide = ({ user }) => {
                         <p className="user-info"><strong>Role:</strong> {user.role}</p>
                     </div>
                     <div>
-                        <button className='left-side-button' >Verify</button>
+                        <button className='left-side-button'>Verify</button>
+                        <button className='left-side-button'>Reject</button>
+                    </div>
+                </div>
+            ))}
+            {user.role === 'jobseeker' && user.jobseeker.length > 0 && user.jobseeker.map((jobseeker, index) => (
+                <div key={index} className='employer-left-side'>
+                    <div className="profile-pic">
+                        <img src={'/jobseeker.png'} alt={`${user.username}`} className="employer-picture"/>
+                        <p className="verification-status">
+                            Verification: {user.profile_verified ? 'Verified' : 'Pending'}
+                        </p>
+                    </div>
+                    <div className="user-details">
+                        <p className="user-info"><strong>Username:</strong> {user.username}</p>
+                        <p className="user-info"><strong>Email:</strong> {user.email}</p>
+                        <p className="user-info"><strong>Phone Number:</strong> {user.phone_number}</p>
+                        <p className="user-info"><strong>Role:</strong> {user.role}</p>
+                    </div>
+                    <div>
+                        <button className='left-side-button'>Verify</button>
+                        <button className='left-side-button'>Reject</button>
+
                     </div>
                 </div>
             ))}
